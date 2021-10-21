1e43ba2d33a08e7838bb8665b889f29c0bdf6af3
op23n1 <amy@op23n1.localdomain>
2021-10-21 13:38:07 -0600

    Added Profile and ProfileList abstract classes

---
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/Profile.java
@@ -0,0 +1,13 @@
+package com.example.habittracker;
+
+public abstract class Profile {
+    protected String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
