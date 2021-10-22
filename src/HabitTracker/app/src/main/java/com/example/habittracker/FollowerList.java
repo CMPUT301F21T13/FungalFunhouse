8b9a42dd9471301efb5c9012ded41869b874b025
op23n1 <amy@op23n1.localdomain>
2021-10-22 12:10:22 -0600

    generated UserProfile and FollowerList classes

---
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/FollowerList.java
@@ -0,0 +1,9 @@
+package com.example.habittracker;
+
+public class FollowerList extends ProfileList{
+    /*
+    * Handles concerns specific to lists of followers
+    * */
+
+
+}
---
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/UserProfile.java
@@ -0,0 +1,24 @@
+package com.example.habittracker;
+
+public class UserProfile extends Profile{
+    private FollowerList following;
+    private FollowerList followers;
+
+    public void followUser(UserProfile profile){
+        following.addProfile(profile);
+    }
+
+    public void unfollowUser(UserProfile profile){
+        following.removeProfile(profile);
+    }
+
+    public void addFollower(UserProfile profile){
+        followers.addProfile(profile);
+    }
+
+    public void removerFollower(UserProfile profile){
+        followers.removeProfile(profile);
+    }
+
+
+}
