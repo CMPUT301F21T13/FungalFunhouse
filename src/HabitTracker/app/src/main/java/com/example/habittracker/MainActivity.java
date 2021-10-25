43881ae9ba622bb0cefa066af1f3d55a96be06d4
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-10-25 14:46:07 -0600

    added profile content and grid for MainActivity

--- src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
@@ -3,14 +3,18 @@ package com.example.habittracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView profileGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        GridView profileGrid = findViewById(R.id.profile_grid);
 
 
     }
