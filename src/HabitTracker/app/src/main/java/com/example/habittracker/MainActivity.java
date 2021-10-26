a3e3017e24342a4d335f1a4891db17cec5619f3e
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-10-26 15:29:09 -0600

    Added onClickItem for profile grid

--- src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.habittracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
@@ -19,13 +22,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         GridView profileGrid = findViewById(R.id.profile_grid);
         profileList = new ArrayList<>();
-
-
         profileArrayAdapter = new ProfileListAdapterGrid(this, profileList);
 
+        profileGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+                Intent intent = new Intent(view.getContext(), HomeTabActivity.class);
 
-    }
+                startActivity(intent);
+
+            }
+        });
+        }
 }
