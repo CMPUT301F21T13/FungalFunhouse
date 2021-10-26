5c38f8f8f517a9959a37254796a901440fa0b15f
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-10-26 15:13:39 -0600

    Added profile adapter for grid

--- src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.habittracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     GridView profileGrid;
+    ArrayList<UserProfile> profileList;
+    ArrayAdapter<UserProfile> profileArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         GridView profileGrid = findViewById(R.id.profile_grid);
+        profileList = new ArrayList<>();
+
+
+        profileArrayAdapter = new ProfileListAdapterGrid(this, profileList);
+
 
 
     }
