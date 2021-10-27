1ede4679160d32e2fe1cf625a3fd7d2220332986
Genna C <genna@ualberta.ca>
2021-10-27 04:49:04 -0600

    Updating FriendsFragment to show future process for code
    Wrote in a general description/guide to how code is likely going to be implemented

--- src/HabitTracker/app/src/main/java/com/example/habittracker/FriendsFragment.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/FriendsFragment.java
@@ -10,4 +10,20 @@ public class FriendsFragment extends Fragment {
     public FriendsFragment(){
         super(R.layout.friends_fragment);
     }
+
+
+    // Implement new Friends activity that shows all following
+        // how do I show followers!!!!!
+
+    // Selecting a user (from a listview) sends into new Activity
+        // has to be new activity unless fragments are re-thought
+
+    // selecting request leads to new activity
+        // create new request inbox if none exists for user
+        // followers are shown using general profileList
+        // FollowRequestInbox.addRequest (request)
+
+    // Selecting Mail leads to mew activity
+        // must have a pre-existing request inbox to show anything
+        // FollowerRequestInbox.acceptRequest or .denyRequest
 }
