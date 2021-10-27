27d19b5eb13974d4919272afe5ae44dc825c74c4
John Macdonald <macjohnny56@gmail.com>
2021-10-27 10:53:06 -0600

    added some hardcoded test users, class now can check if user exists or not and act accordingly

--- src/HabitTracker/app/src/main/java/com/example/habittracker/LogInActivity.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/LogInActivity.java
@@ -3,6 +3,7 @@ package com.example.habittracker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,23 +29,34 @@ public class LogInActivity extends AppCompatActivity {
         TextView forgotPassword = (TextView) findViewById(R.id.forgotpassword);
         MaterialButton loginButton = (MaterialButton) findViewById(R.id.loginbutton);
 
+        profileList = new ArrayList<>();
+        UserProfile user1 = new UserProfile("user1");
+        UserProfile user2 = new UserProfile("user2");
+        profileList.add(user1);
+        profileList.add(user2);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                UserProfile user = new UserProfile(username.getText().toString());
-
-                if (username.getText().toString().equals("funnybot") && password.getText().toString().equals("password")) {
-                    // correct
-                    Toast.makeText(LogInActivity.this, "login successful", Toast.LENGTH_SHORT).show();
-
-                    Intent intent = new Intent(LogInActivity.this, HomeTabActivity.class);
-                    intent.putExtra("user", (Parcelable) user);
-                    startActivity(intent);
-                } else {
-                    //incorrect
+                int flag = 0;
+                for (UserProfile i : profileList) {
+                    UserProfile user = i;
+//                    Log.i("value of username: ", aUsername); // test to check value of string aUsername using Log class
+                    if (user.getUsername().equals(username.getText().toString())) {
+                        // user exists
+                        flag = 1;
+                        //                    profileList.get()
+                        //                    profileList.indexOf();
+                        Toast.makeText(LogInActivity.this, "login successful", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(LogInActivity.this, HomeTabActivity.class);
+                        intent.putExtra("user", (Parcelable) user);
+                        startActivity(intent);
+                    }
+                }
+                if (flag == 0) {
+                    //user doesn't exist / can't be found
                     Toast.makeText(LogInActivity.this, "login failed", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
 
