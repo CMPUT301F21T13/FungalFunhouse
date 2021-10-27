b7be8037ceb029e47a650a893044109d9cae302e
Glenn Storch <storch@ualberta.ca>
2021-10-26 20:32:28 -0600

    adding comments for future self

--- src/HabitTracker/app/src/main/java/com/example/habittracker/HabitsFragment.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/HabitsFragment.java
@@ -10,4 +10,12 @@ public class HabitsFragment extends Fragment {
     public HabitsFragment() {
         super(R.layout.habit_fragment);
     }
+
+    /*TODO(GLENN): Create logic for Habits fragment including: selecting a habit, add button,
+       edit button, delete button, and visual indicator.
+
+       After this move onto the activities for add habit and edit habit (should be able to reuse
+       the same activity just with a different data Bundle.
+     */
+
 }
