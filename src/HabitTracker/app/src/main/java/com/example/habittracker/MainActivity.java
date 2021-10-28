c278112b982a89984fc49180cdedda88af70f90b
John Macdonald <macjohnny56@gmail.com>
2021-10-28 14:27:29 -0600

    changed to start new activity on create

--- src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/MainActivity.java
@@ -1,41 +1,31 @@
 package com.example.habittracker;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    GridView profileGrid;
-    ArrayList<UserProfile> profileList;
-    ArrayAdapter<UserProfile> profileArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        GridView profileGrid = findViewById(R.id.profile_grid);
-        profileList = new ArrayList<>();
-        profileArrayAdapter = new ProfileListAdapterGrid(this, profileList);
-
-        profileGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                Intent intent = new Intent(view.getContext(), HomeTabActivity.class);
-
-                startActivity(intent);
-
-            }
-        });
-        }
+        startActivity(new Intent(MainActivity.this, LogInActivity.class));
+        finish();
+        
+    }
 }
