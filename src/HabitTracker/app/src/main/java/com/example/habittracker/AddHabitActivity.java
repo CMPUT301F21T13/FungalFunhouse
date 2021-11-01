19b5128fba67db3cc2d23a612c103331da833806
Glenn Storch <storch@ualberta.ca>
2021-10-31 19:27:33 -0600

    Added xml for adding/editing habits

---
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/AddHabitActivity.java
@@ -0,0 +1,13 @@
+package com.example.habittracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * This is the activity used for adding new habits to the UserProfile habitlist
+ * to be displayed in the Habits tab of HomeTabActivity
+ */
+public class AddHabitActivity extends AppCompatActivity {
+
+
+
+}
