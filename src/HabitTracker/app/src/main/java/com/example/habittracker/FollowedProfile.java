d1d7da519ef0a598563a7d9b06752c8fb253cd7c
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-11-03 10:18:38 -0600

    Removed references to habits, change in implementation

--- src/HabitTracker/app/src/main/java/com/example/habittracker/FollowedProfile.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/FollowedProfile.java
@@ -1,9 +1,7 @@
 package com.example.habittracker;
 
-import java.util.ArrayList;
 
 public class FollowedProfile extends Profile{
-    private HabitList habitList;
     private String follwer;
 
 
@@ -12,14 +10,6 @@ public class FollowedProfile extends Profile{
     }
     public void setFollower(String username){
         follwer = username;
-    }
-
-    public ArrayList<Habit> getHabits(){
-        return habitList.getList();
-    }
-
-    public void setHabits(HabitList habits){
-        habitList = habits;
     }
 
 }
