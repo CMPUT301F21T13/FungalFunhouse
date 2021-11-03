c19e85823cbdac1314f649db8c6e47c3bdbd701b
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-11-03 09:36:03 -0600

    Created serializeProfile() and implemented variables

--- src/HabitTracker/app/src/main/java/com/example/habittracker/Serialization.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/Serialization.java
@@ -1,6 +1,25 @@
+package com.example.habittracker;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+
 public class Serialization {
+
+	FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 	/* Class will contain the methods for 
 	 * I/O with the firestore
 	 * Mainly Profile construction and deconstruction
 	 */
+
+	public static void serializeUserProfile(UserProfile user){
+		ArrayList<Profile> followers = user.getFollowers();
+		ArrayList<Profile> following = user.getFollowing();
+		ArrayList<FollowRequest> requestInbox = user.getRequests();
+		ArrayList<Habit> habits = user.getHabits();
+
+
+
+	}
 }
