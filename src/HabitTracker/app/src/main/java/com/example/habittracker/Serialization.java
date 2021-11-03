3a6b4e7674070a28bdc9231cef20ffb3897c1188
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-11-03 10:35:21 -0600

    implemented database insertion for UserProfiles

--- src/HabitTracker/app/src/main/java/com/example/habittracker/Serialization.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/Serialization.java
@@ -1,19 +1,28 @@
 package com.example.habittracker;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Serialization {
 
 	static FirebaseFirestore db = FirebaseFirestore.getInstance();
+	static final String TAG = "Sanple";
 
 	/* Class will contain the methods for 
 	 * I/O with the firestore
 	 * Mainly Profile construction and deconstruction
 	 */
 
-	public static void serializeUserProfile(UserProfile user){
+	public static <OnFailureListener> void serializeUserProfile(UserProfile user){
 		ArrayList<Profile> followers = user.getFollowers();
 		ArrayList<Profile> following = user.getFollowing();
 		ArrayList<FollowRequest> requestInbox = user.getRequests();
@@ -24,6 +33,26 @@ public class Serialization {
 		ArrayList<String> followingNames = getProfileNames(following);
 		ArrayList<String> followRequestSenders = getProfileNames(getRequestSenders(requestInbox));
 
+		Map<String, Object> profile = new HashMap<>();
+		profile.put("username", user.getUsername());
+		profile.put("followers", followerNames);
+		profile.put("following", followingNames);
+		profile.put("follow-requests", followRequestSenders);
+
+		db.collection("UserProfiles").document(username)
+				.set(profile)
+				.addOnSuccessListener(new OnSuccessListener<Void>() {
+					@Override
+					public void onSuccess(Void aVoid) {
+						Log.d(TAG, "Profile successfully written!");
+					}
+				})
+				.addOnFailureListener(new OnFailureListener() {
+					@Override
+					public void onFailure(@NonNull Exception e) {
+						Log.w(TAG, "Profile writing document", e);
+					}
+				});
 
 	}
 
