00ceec85e424bfc677adb45af2f3a64ef635750f
John Macdonald <macjohnny56@gmail.com>
2021-11-05 09:53:04 -0600

    added skeleton code for some basic tests

--- src/HabitTracker/app/src/androidTest/java/com/example/habittracker/CreateUserTest.java
+++ src/HabitTracker/app/src/androidTest/java/com/example/habittracker/CreateUserTest.java
@@ -1,4 +1,42 @@
 package com.example.habittracker;
 
+import androidx.test.espresso.intent.Intents;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+
 public class CreateUserTest {
+
+    @Rule
+    public ActivityScenarioRule<CreateUser> activityRule = new ActivityScenarioRule<>(CreateUser.class);
+
+    @Before
+    public void setUp() throws Exception {
+        Intents.init();
+    }
+
+
+    @Test
+    public void testMissingUsernameScenario() {
+
+    }
+
+    @Test
+    public void testMissingPasswordScenario() {
+        
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        Intents.release();
+    }
 }
