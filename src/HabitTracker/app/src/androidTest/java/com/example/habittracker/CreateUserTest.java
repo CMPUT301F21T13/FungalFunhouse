c6f6edf6b3206551e858cb8cb4c2a4fa04976012
John Macdonald <macjohnny56@gmail.com>
2021-11-05 10:15:59 -0600

    added, tested, passed testMissingUsernameScenario() and testMissingPasswordScenario()

--- src/HabitTracker/app/src/androidTest/java/com/example/habittracker/CreateUserTest.java
+++ src/HabitTracker/app/src/androidTest/java/com/example/habittracker/CreateUserTest.java
@@ -1,5 +1,12 @@
 package com.example.habittracker;
 
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -19,6 +26,11 @@ public class CreateUserTest {
     @Rule
     public ActivityScenarioRule<CreateUser> activityRule = new ActivityScenarioRule<>(CreateUser.class);
 
+    private String takenUsername = "mockUser";
+    private String freeUsername = "newUser";
+    private String password = "password1";
+
+
     @Before
     public void setUp() throws Exception {
         Intents.init();
@@ -27,12 +39,22 @@ public class CreateUserTest {
 
     @Test
     public void testMissingUsernameScenario() {
-
+        // click sign up button
+        Espresso.onView(withId(R.id.signupbutton)).perform(click());
+        // check if correct error message is displayed
+        Espresso.onView(withId(R.id.setusername)).check(matches(hasErrorText("Username required")));
     }
 
     @Test
     public void testMissingPasswordScenario() {
-        
+        // input some username in username field
+        Espresso.onView(withId(R.id.setusername)).perform(typeText(takenUsername));
+        //close soft keyboard
+        Espresso.closeSoftKeyboard();
+        // click sign up button
+        Espresso.onView(withId(R.id.signupbutton)).perform(click());
+        // check if correct error message is displayed
+        Espresso.onView(withId(R.id.setpassword)).check(matches(hasErrorText("Password required")));
     }
 
     @After
