70e1df5882de52fda5871f46abc70a6aa7c8184c
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-11-23 15:10:02 -0700

    Added generic event generation function by week

--- src/HabitTracker/app/src/main/java/com/example/habittracker/HabitEventList.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/HabitEventList.java
@@ -1,6 +1,11 @@
 package com.example.habittracker;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 public class HabitEventList {
@@ -21,8 +26,40 @@ public class HabitEventList {
     public void setList(ArrayList<HabitEvent> eventList) {
         this.habitEventList = eventList;
     }
-    public void generateWeekEvents(Date weekStart){
-        WeeklySchedule week =  modelHabit.getWeeklySchedule();
 
+    //week starts on sunday
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void generateWeekEvents(int year, int weekStart){
+        WeeklySchedule week =  modelHabit.getWeeklySchedule();
+        Calendar date = Calendar.getInstance();
+        date.setLenient(true);
+        if(week.checkSunday()){
+            date.setWeekDate(year, weekStart, 1);
+            this.addEvent(new HabitEvent(date));
+        }
+        if(week.checkMonday()){
+            date.setWeekDate(year, weekStart, 2);
+            this.addEvent(new HabitEvent(date));
+        }
+        if(week.checkTuesday()){
+            date.setWeekDate(year, weekStart, 3);
+            this.addEvent(new HabitEvent(date));
+        }
+        if(week.checkWednesday()){
+            date.setWeekDate(year, weekStart, 4);
+            this.addEvent(new HabitEvent(date));
+        }
+        if(week.checkThursday()){
+            date.setWeekDate(year, weekStart, 5);
+            this.addEvent(new HabitEvent(date));
+        }
+        if(week.checkFriday()){
+            date.setWeekDate(year, weekStart, 6);
+            this.addEvent(new HabitEvent(date));
+        }
+        if(week.checkSaturday()){
+            date.setWeekDate(year, weekStart, 7);
+            this.addEvent(new HabitEvent(date));
+        }
     }
 }
