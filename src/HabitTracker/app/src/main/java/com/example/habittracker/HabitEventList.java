d2e812aa5ea87989fb9eb6865a9e2c65a2e96cee
op23n1 <60111606+op23n1@users.noreply.github.com>
2021-11-23 14:47:05 -0700

    Created attributes and methods per UML perscription

--- src/HabitTracker/app/src/main/java/com/example/habittracker/HabitEventList.java
+++ src/HabitTracker/app/src/main/java/com/example/habittracker/HabitEventList.java
@@ -1,4 +1,28 @@
 package com.example.habittracker;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class HabitEventList {
+    private Habit modelHabit;
+    private ArrayList<HabitEvent> habitEventList;
+
+    public void addEvent(HabitEvent habitEvent){
+        habitEventList.add(habitEvent);
+    }
+    public void removeEvent(HabitEvent habitEvent){
+        habitEventList.remove(habitEvent);
+    }
+
+    public ArrayList<HabitEvent> getList() {
+        return habitEventList;
+    }
+
+    public void setList(ArrayList<HabitEvent> eventList) {
+        this.habitEventList = eventList;
+    }
+    public void generateWeekEvents(Date weekStart){
+        WeeklySchedule week =  modelHabit.getWeeklySchedule();
+
+    }
 }
