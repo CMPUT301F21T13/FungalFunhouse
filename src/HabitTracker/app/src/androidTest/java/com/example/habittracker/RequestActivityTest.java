5acef3d5415f20f268df5e7ff39946b2c3e1968e
Genna-C <genna@ualberta.ca>
2021-11-24 16:23:13 -0700

    requestActivityTest is now fully functional
    Finished Tests, will add more in terms of dealing with invalid inputs.
    Also modified some xml labels for better clarification

--- src/HabitTracker/app/src/androidTest/java/com/example/habittracker/RequestActivityTest.java
+++ src/HabitTracker/app/src/androidTest/java/com/example/habittracker/RequestActivityTest.java
@@ -1,21 +1,28 @@
 package com.example.habittracker;
 
-import android.app.Activity;
-
+import static android.service.autofill.Validators.not;
 import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.espresso.intent.Intents.intended;
-
 import static org.hamcrest.CoreMatchers.anything;
+import static org.hamcrest.Matchers.not;
 
+import android.content.Intent;
+import android.service.autofill.Validator;
+import android.view.View;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -34,31 +41,71 @@ import org.junit.runner.RunWith;
 
 public class RequestActivityTest {
 
+    private View decorView;
+    static Intent intent;
+    static {
+        intent = new Intent(ApplicationProvider.getApplicationContext(), RequestActivity.class);
+        intent.putExtra("user", "testUser1");
+    }
+
     @Rule
-    public ActivityScenarioRule<RequestActivity> activityRule = new ActivityScenarioRule<>(RequestActivity.class);
+    public ActivityScenarioRule<RequestActivity> activityRule = new ActivityScenarioRule<>(intent);
 
     @Before
     public void setUp() throws Exception {
         Intents.init();
+        activityRule.getScenario().onActivity(
+                activity -> decorView = activity.getWindow().getDecorView()
+        );
     }
 
+
     /**
-     * Checks that filtered users are being displayed
+     * Checks that users are being displayed
+     * @throws Exception
      */
     @Test
-    public void testUserList(){
-        Espresso.onView(withId(R.id.username_input)).perform(typeText("mockUser1"));
+    public void testUserList() throws Exception{
+        Thread.sleep(2000);
+        Espresso.onView(withText("testUser2")).check(matches(isDisplayed()));
+    }
+
+
+    /**
+     * Checks if the search_input correctly
+     * Filters and displays individuals
+     * @throws Exception
+     */
+    @Test
+    public void testFilterUsers() throws Exception{
+        //Enter the filter "testUser2"
+        Espresso.onView(withId(R.id.username_input)).perform(clearText()).perform(typeText("testUser2"));
         Espresso.onView(withId(R.id.search_button)).perform(click());
-        Espresso.onView(withText("mockUser1")).check(matches(isDisplayed()));
+        Thread.sleep(1000);
+
+        //check if the list has filtered
+        onData(anything()).inAdapterView(withId(R.id.user_search_list)).atPosition(0)
+                .onChildView(withId(R.id.username_text_grid))
+                .check(matches(withText("testUser2")));
+        Espresso.onView(withText("mockUser")).check(doesNotExist());
     }
 
+
     /**
-     * Checks that a Toast pops up after a request is sent
+     * Check that sending a request works
+     * @throws Exception
      */
     @Test
-    public void testSendRequest(){
-        onData(anything()).atPosition(0).perform(click());
-        Espresso.onView(withText("Follow Request Sent")).check(matches(isDisplayed()));
+    public void testSendRequest() throws Exception{
+        Thread.sleep(1000);
+
+        //Filter and select User "testUser2"
+        Espresso.onView(withId(R.id.username_input)).perform(clearText()).perform(typeText("testUser2"));
+        Espresso.onView(withId(R.id.search_button)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.user_search_list)).atPosition(0)
+                .onChildView(withId(R.id.username_text_grid))
+                .perform(click());
+        Espresso.onView(withId(R.id.enter_button)).perform(click());
 
     }
 
